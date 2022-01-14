60c93ee0d643fd3d2cbecb4577a84d46ec748fc3
Rahul Tarak <cryogenicplanet@gmail.com>
2022-01-15 03:05:04 +0530

    ✨ Add express.zip

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -3,3 +3,4 @@ cd templates/basic && zip -r ../basic.zip .
 cd ../vite && zip -r ../vite.zip .
 cd ../next && zip -r ../next.zip .
 cd ../vite-library-mode && zip -r ../viteLib.zip .
+cd ../express && zip -r ../express.zip .
